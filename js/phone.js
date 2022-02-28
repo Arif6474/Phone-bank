6d573539728dc4572f6194cba06081b96639db79
Arif6474 <arifulislam64743@gmail.com>
2022-03-01 02:45:52 +0600

    second commit

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,11 @@
-console.log('phone checked');
+const searchButton = ()=>{
+    const phoneInput = document.getElementById('phone-input') ;
+    const phoneValue = phoneInput.value;
+    const error = document.getElementById('error');
+    if (phoneValue === '') {
+        error.innerText = 'Please enter a phone';
+
+    }
+    phoneInput.value = '';
+
+}
