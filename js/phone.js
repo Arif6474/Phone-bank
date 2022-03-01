b0fcdd5399f92713f00d63f8c5cd336d6324f7b8
Arif6474 <arifulislam64743@gmail.com>
2022-03-01 23:42:46 +0600

    fifth commit

--- js/phone.js
+++ js/phone.js
@@ -9,28 +9,26 @@ const searchButton = ()=>{
         error.innerText = 'Please enter a phone';
         allPhones.innerHTML = '';
 
-    } 
-    // else if (isNaN(phoneValue) ) {
-    //     error.innerText = 'No result found'
-    // }
+    }  
     else {
+        allPhones.innerHTML = '';
         fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneValue}`)
         .then(res => res.json())
-        .then(data => displayPhones(data.data))
+        .then(data => displayPhones(data.data));
         error.innerText = '';
     }
-
     phoneInput.value = '';
 
 }
 const displayPhones = phones => {
     const first20Phone = phones.slice(0, 20);
-    // console.log(first20Phone);
+    // console.log(phones);
 
     const allPhones = document.getElementById('all-phone');
     for (const phone of first20Phone) {
-        // console.log(phone);
-    
+       
+    // console.log(phone);
+
     const div = document.createElement('div');
     div.classList.add('col-lg-4')
     div.classList.add('my-4')
@@ -38,9 +36,9 @@ const displayPhones = phones => {
     <div class="card" style="width: 18rem;">
           <img src="${phone.image}" class="card-img-top" alt="...">
       <div class="card-body">
-          <h5>${phone.phone_name}</h5>
+          <h5>${phone.phone_name?name:"no result found"}</h5>
         <p>Brand: ${phone.brand}</p>
-        <button class="btn btn-primary">Details</button>
+        <button onclick = "phoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
       </div>
     </div>
     `
@@ -49,3 +47,41 @@ const displayPhones = phones => {
     }
     
 }
+
+const phoneDetails = (singlePhone) => {
+    // console.log(singlePhone);
+    fetch(`https://openapi.programming-hero.com/api/phone/${singlePhone}`)
+        .then(res => res.json())
+        .then(data => displaySinglePhone(data.data));
+ 
+}
+
+const displaySinglePhone = details => {
+    console.log(details);
+    allPhones.innerHTML = '';
+    const div = document.createElement('div');
+    div.classList.add('d-flex')
+    div.classList.add('justify-content-center')
+    div.innerHTML = `
+    <div class="card mb-3" >
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${details.name}</h5>
+          <p class="card-text">${details.slug}</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+      `
+     allPhones.appendChild(div);
+
+
+}
+
+
+
